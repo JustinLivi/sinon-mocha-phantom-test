531ff9443212fd95d500bf819d870a1fbe9792ab
jlivi <jlivi@millennialmedia.com>
2015-08-10 09:51:19 -0400

    Added Test Illustrating Workaround
    Restoring the clock before ending the test allows the promise to resolve.

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 (function() {
 
+	// Works as expected
 	describe('chai as promised', function() {
 
 		it('should resolve a promise', function() {
@@ -11,6 +12,7 @@
 
 	});
 	
+	// Fails
 	describe('sinon.useFakeTimers()', function() {
 
 		before(function() {
@@ -28,6 +30,27 @@
 			});
 
 			this.clock.tick( 1001 );
+
+			return expect( promise ).to.have.been.fulfilled;
+		});
+
+	});
+
+	// Workaround - restore the timer before completion of the test
+	describe('sinon.useFakeTimers()', function() {
+
+		before(function() {
+			this.clock = sinon.useFakeTimers();
+		});
+
+		it('should resolve a promise after ticking', function() {
+
+			var promise = new Promise(function( resolve ) {
+				setTimeout( resolve, 1000 );
+			});
+
+			this.clock.tick( 1001 );
+			this.clock.restore();
 
 			return expect( promise ).to.have.been.fulfilled;
 		});
